4ce3910cbfaacb521eecefda97684e407d7a58be
Muga Nishizawa <muga.nishizawa@gmail.com>
2017-05-13 00:35:25 -0700

    Use TDClient#appendTableSchema for import_only account's write_only key

--- src/main/java/org/embulk/output/td/TdOutputPlugin.java
+++ src/main/java/org/embulk/output/td/TdOutputPlugin.java
@@ -748,7 +748,7 @@ public class TdOutputPlugin
             newSchema.add(new TDColumn(pair.getKey(), pair.getValue(), key.getBytes(StandardCharsets.UTF_8)));
         }
 
-        client.updateTableSchema(databaseName, task.getLoadTargetTableName(), newSchema);
+        client.appendTableSchema(databaseName, task.getLoadTargetTableName(), newSchema);
         return guessedSchema;
     }
 
